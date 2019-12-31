6d8c5a43beab1fd3447da651b48e1181a798f9a7
Percy Mugadza <percym@localhost.localdomain>
2019-12-31 08:20:19 +0200

    init commit

---
+++ app/src/main/java/zw/co/researchhub/happyfirst/MainActivity.java
@@ -0,0 +1,27 @@
+package zw.co.researchhub.happyfirst;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button submitButton;
+        submitButton = findViewById(R.id.submit_btn);
+        submitButton.setOnClickListener(v -> {
+
+            Intent i = new Intent(MainActivity.this, Welcome.class);
+            startActivity(i);
+        });
+
+    }
+}
